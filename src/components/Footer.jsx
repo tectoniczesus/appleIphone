42aac861659025f990fe810903ea1abff855338f
tectoniczesus <jp6714410@gmail.com>
2024-12-06 17:30:13 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { footerLinks } from '../contants'
+
+const footer = () => {
+  return (
+    <footer className='py-5 sm:px-10 px-5'>
+      <div className='screen-max-width'>
+        <div>
+          <p className='font-semibold text-gray text-xs'>
+            More ways to shop: {' '}
+            <span className='underline text-blue'>Find an Apple Store {' '}</span>
+            or {' '}
+            <span className='underline text-blue'>other retailer</span>
+            <span>{' '} 
+              near you.</span>
+            </p>
+            <p className='font-semibold text-gray text-xs'>Or call 000800-040-1966</p>
+        </div>
+        <div className='bg-neutral-700 my-5 h-[1px] w-full'/>
+        
+          <div className='flex md:flex-row flex-col md:items-center justify-between'>
+            <p className='font-semibold text-gray text-xs'> 
+              Copyright @ 2024 Apple Inc. All rights reversed.</p>
+              <div className='flex'>
+                {footerLinks.map((link,i)=>(
+                  <p key={link} className='font-semibold text-gray text-xs'> {link} {''}
+                  {i!== footerLinks.length-1 && (
+                    <span className='mx-2'> | </span>
+                  )}
+                  </p>
+                ))}
+              </div>
+          </div>
+        </div>
+    
+    </footer>
+  )
+}
+
+export default footer
